f226d034af84e1a5471e8d468ff3d2954352ad74
Steve Grossi <me@stevegrossi.com>
2016-10-29 06:40:12 -0400

    Install GenStage

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Lumberjack.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :gen_stage]]
+    [applications: [:logger, :gen_stage],
+     mod: {Lumberjack, []}]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +28,6 @@ defmodule Lumberjack.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:gen_stage, "~> 0.7"}]
   end
 end
