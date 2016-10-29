0ea74b572e94dc656030b6c4d40a8c8da7f47d8b
Steve Grossi <me@stevegrossi.com>
2016-10-29 07:06:46 -0400

    Automatically detect appropriate number of consumers
    ```
    System.schedulers_online #=> 4
    ```
    
    Even though I have only 2 cores. Hyper threading?
    
    Also, this is amazing.

--- lib/lumberjack.ex
+++ lib/lumberjack.ex
@@ -9,15 +9,15 @@ defmodule Lumberjack do
     # Define workers and child supervisors to be supervised
     children = [
       worker(Lumberjack.Reader, [0]),
-      worker(Lumberjack.Printer, [], id: 1),
-      worker(Lumberjack.Printer, [], id: 2),
-      worker(Lumberjack.Printer, [], id: 3),
-      worker(Lumberjack.Printer, [], id: 4)
     ]
+    consumers =
+      for id <- 1..System.schedulers_online do
+        worker(Lumberjack.Printer, [], id: id)
+      end
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Lumberjack.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(children ++ consumers, opts)
   end
 end
